18ce07fde9e715d8daa4e2e854baa63391f3066a
gwbasic <kebechsenuef@users.noreply.github.com>
2019-06-03 22:37:09 +0200

    Update of questions with Freund common working areas
    Source: book ISBN 9783596901715, page 9

--- index.php
+++ index.php
@@ -12,22 +12,39 @@
 			$inhalte[].="art";
 			$inhalte[].="economy";
 			$inhalte[].="politics";
-			$inhalte[].="social/life";
+			$inhalte[].="social/life/Society";
 			$inhalte[].="science";
 			$inhalte[].="work";
 			$inhalte[].="sport";
 			$inhalte[].="(your current town/area)";
 			$inhalte[].="technic";
+			$inhalte[].="dinner";
+			$inhalte[].="music";
+			$inhalte[].="climate";
+			$inhalte[].="minority";
+
+			
+			//continents
 			$inhalte[].="asia";
 			$inhalte[].="africa";
 			$inhalte[].="south-america";
 			$inhalte[].="north-america";
 			$inhalte[].="australia";
 			$inhalte[].="europe";
-			$inhalte[].="dinner";
-			$inhalte[].="music";
-			$inhalte[].="climate";
-			$inhalte[].="minority";
+
+			//freud
+			$inhalte[].="daily life";
+			$inhalte[].="love";
+			$inhalte[].="war";
+			$inhalte[].="death";
+			$inhalte[].="(un)aware";
+			$inhalte[].="normality and neurose";
+			$inhalte[].="literature";
+			$inhalte[].="legend";
+			$inhalte[].="fairytale";
+			$inhalte[].="religion";
+			$inhalte[].="culture";
+			
 
 			$ende=count($inhalte)-1;
 
